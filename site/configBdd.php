6fa5a55fb5a3052e8401cda77920e2ce8b3b5a9f
YDogny <yanndogny47@gmail.com>
2025-11-06 16:30:30 +0000

    Modif configBdd.php

--- site/configBdd.php
+++ site/configBdd.php
@@ -1,8 +1,8 @@
 <?php
-$_ENV['bd'] = 'tp_sio2_bdjourneeintegration';
+$_ENV['bd'] = 'bd_projetfa';
 
-$_ENV['local_dsn'] = 'mysql:host=127.0.0.1;dbname=' . $_ENV['bd'] . ';port=3306';
+$_ENV['host'] = 'localhost';
 
-$_ENV['remote_dsn_slam'] = 'mysql:host=194.199.35.4;dbname=' . $_ENV['bd'] . ';port=3306';
+$_ENV['username'] = 'root';
 
-$_ENV['options'] = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'');
+$_ENV['mdp'] = '';
