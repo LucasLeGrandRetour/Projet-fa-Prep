7d0d17b6e2a1f5870410480bc7b634a2c2e4f19a
YDogny <yanndogny47@gmail.com>
2025-11-06 15:07:10 +0000

    remove all

--- site/modeles/testBaseDeveloppeurDAO.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once 'BaseDeveloppeurDAO.php';
-include_once '../configBdd.php';
-$connexionDev = new BaseDeveloppeurDAO();
-$lesDeveloppeurs = $connexionDev->getLesDeveloppeurs();
-var_dump($lesDeveloppeurs);
