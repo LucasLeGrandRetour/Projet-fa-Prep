530532688e65821f416e781438d93f58cb9510c8
Annaelle972 <140344538+Annaelle972@users.noreply.github.com>
2025-11-13 15:08:20 +0000

    v0-nouvelle BD

--- database/db_projetfa.sql
+++ database/db_projetfa.sql
@@ -1,3 +1,5 @@
+DROP DATABASE IF EXISTS db_projetfa;
+
 CREATE DATABASE IF NOT EXISTS db_projetfa;
 USE db_projetfa;
 
@@ -5,11 +7,16 @@ CREATE TABLE Evenement (
   idEvent INT NOT NULL AUTO_INCREMENT,
   libelleEvent VARCHAR(100) NOT NULL,
   descriptionEvent VARCHAR(500) NOT NULL,
-  dateEvent DATE NOT NULL,
-  heureDebEvent TIME NOT NULL,
-  heureFinEvent TIME NOT NULL,
   capaMaxi INT NOT NULL,
   CONSTRAINT Evenement_PK PRIMARY KEY (idEvent)
+) ENGINE=InnoDB;
+
+CREATE TABLE Horaires (
+  idHoraire INT NOT NULL AUTO_INCREMENT,
+  date DATE NOT NULL,
+  heureDeb TIME NOT NULL,
+  heureFin TIME NOT NULL,
+  CONSTRAINT Horaires_PK PRIMARY KEY (idHoraire)
 ) ENGINE=InnoDB;
 
 CREATE TABLE typeDroit (
@@ -25,12 +32,19 @@ CREATE TABLE Tarif (
   CONSTRAINT Tarif_PK PRIMARY KEY (idTarif)
 ) ENGINE=InnoDB;
 
+CREATE TABLE Concerner (
+  idHoraire INT NOT NULL,
+  idEvent INT NOT NULL,
+  CONSTRAINT Concerner_PK PRIMARY KEY (idHoraire, idEvent),
+  CONSTRAINT Concerner_idHoraire_FK FOREIGN KEY (idHoraire) REFERENCES Horaires (idHoraire),
+  CONSTRAINT Concerner_idEvent_FK FOREIGN KEY (idEvent) REFERENCES Evenement (idEvent)
+) ENGINE=InnoDB;
+
 CREATE TABLE User (
-  idUser INT NOT NULL,
+  idUser INT NOT NULL AUTO_INCREMENT,
   nom VARCHAR(20) NOT NULL,
   prenom VARCHAR(20) NOT NULL,
-  mail VARCHAR(20),
-  telephone VARCHAR(50),
+  mail VARCHAR(20) NOT NULL,
   mdp VARCHAR(20) NOT NULL,
   GDU VARCHAR(50) NOT NULL,
   idDroit INT NOT NULL,
