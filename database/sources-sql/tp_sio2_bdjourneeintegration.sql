5e5ebb59184c02de610d31ea74a8aea4616c855d
YDogny <yanndogny47@gmail.com>
2025-11-20 14:15:34 +0100

    V0.5 maj acces base

---
+++ database/sources-sql/tp_sio2_bdjourneeintegration.sql
@@ -0,0 +1,52 @@
+USE tp_sio2_bdjourneeintegration;
+
+#------------------------------------------------------------
+# Table: Developpeur
+#------------------------------------------------------------
+
+DROP TABLE IF EXISTS `Developpeur`;
+
+CREATE TABLE Developpeur (
+    id int Auto_increment NOT NULL,
+    nom Varchar(20) NOT NULL,
+    prenom Varchar(15) NOT NULL,
+    CONSTRAINT Classe_PK PRIMARY KEY (id)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb3 COLLATE = utf8mb3_general_ci;
+
+INSERT INTO
+    Developpeur
+VALUES ('1', 'COVER', 'Harry'),
+    ('2', 'TOUIL', 'Sacha'),
+    ('3', 'GUETTE', 'Garry');
+
+#------------------------------------------------------------
+# Table: Competence
+#------------------------------------------------------------
+CREATE TABLE Competence(
+        id      Int  Auto_increment  PRIMARY KEY NOT NULL ,
+        nom Varchar (40) NOT NULL
+)ENGINE=InnoDB;
+
+#------------------------------------------------------------
+# Données de départ
+#------------------------------------------------------------
+INSERT INTO Competence(nom) VALUES ('Développement Web- CSS');
+INSERT INTO Competence(nom) VALUES ('Web- CSS');
+INSERT INTO Competence(nom) VALUES ('PHP');
+INSERT INTO Competence(nom) VALUES ('HTML');
+INSERT INTO Competence(nom) VALUES ('Java');
+INSERT INTO Competence(nom) VALUES ('Rien');
+
+
+
+#------------------------------------------------------------
+# Habilitations
+#------------------------------------------------------------
+GRANT SELECT ON `tp_sio2_bdjourneeintegration`.`Developpeur` TO 'JI_Dev_Read'@'%';
+GRANT SELECT ON `tp_sio2_bdjourneeintegration`.`Competence` TO 'JI_Comp_Read'@'%';
+
+GRANT INSERT ON `tp_sio2_bdjourneeintegration`.`Developpeur` TO 'JI_Dev_Write'@'%';
+GRANT UPDATE, SELECT ON `tp_sio2_bdjourneeintegration`.`Developpeur` TO 'JI_Dev_Update'@'%';
+GRANT SELECT, DELETE ON `tp_sio2_bdjourneeintegration`.`Developpeur` TO 'JI_Dev_Delete'@'%';
+GRANT SELECT, DELETE ON `tp_sio2_bdjourneeintegration`.`Competence` TO 'JI_Comp_Delete'@'%';
+GRANT SELECT, UPDATE ON `tp_sio2_bdjourneeintegration`.`Competence` TO 'JI_Comp_Update'@'%';
